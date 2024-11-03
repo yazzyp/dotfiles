09f5482564a828c0d03094ca1095539843e9b6cc
Yazz <67986029+yazzyp@users.noreply.github.com>
2024-11-03 15:28:03 +1100

    Update lspconfig.lua

--- .chezmoitemplates/nvim/lua/configs/lspconfig.lua
+++ .chezmoitemplates/nvim/lua/configs/lspconfig.lua
@@ -4,7 +4,7 @@ require("nvchad.configs.lspconfig").defaults()
 local lspconfig = require "lspconfig"
 
 -- EXAMPLE
-local servers = { "html", "cssls", "pyright", "bashls" }
+local servers = { "html", "cssls", "pyright", "bashls", "yamlls", "jsonls", "lemminx" }
 local nvlsp = require "nvchad.configs.lspconfig"
 
 -- lsps with default config
@@ -22,3 +22,29 @@ end
 --   on_init = nvlsp.on_init,
 --   capabilities = nvlsp.capabilities,
 -- }
+
+-- C# omnisharp
+lspconfig.omnisharp.setup {
+  on_attach = nvlsp.on_attach,
+  on_init = nvlsp.on_init,
+  capabilities = nvlsp.capabilities,
+  cmd = { "dotnet", vim.fn.stdpath "data" .. "/mason/packages/omnisharp/libexec/OmniSharp.dll" },
+
+  settings = {
+    FormattingOptions = {
+      EnableEditorConfigSupport = true,
+      OrganizeImports = true,
+    },
+    MsBuild = {
+      -- LoadProjectsOnDemand = nil,
+    },
+    RoslynExtensionsOptions = {
+      EnableAnalyzersSupport = true,
+      EnableImportCompletion = true,
+      -- AnalyzeOpenDocumentsOnly = true,
+    },
+    Sdk = {
+      IncludePrereleases = true,
+    },
+  },
+}
