555932cf992235c76375822a037fc70877525513
Yazz <67986029+yazzyp@users.noreply.github.com>
2024-11-03 15:32:22 +1100

    Update options.lua

--- .chezmoitemplates/nvim/lua/options.lua
+++ .chezmoitemplates/nvim/lua/options.lua
@@ -3,7 +3,9 @@ require "nvchad.options"
 -- add yours here!
 
 local opt = vim.opt
--- local o = vim.o
+local o = vim.o
 -- o.cursorlineopt ='both' -- to enable cursorline!
 
 opt.rnu = true
+
+o.tabstop = 4
