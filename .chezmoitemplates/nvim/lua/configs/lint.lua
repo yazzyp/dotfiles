72c64a4cff7836a4e9043d7879b51d53c388e8c6
Yazz <67986029+yazzyp@users.noreply.github.com>
2024-11-03 15:27:24 +1100

    Update lint.lua

--- .chezmoitemplates/nvim/lua/configs/lint.lua
+++ .chezmoitemplates/nvim/lua/configs/lint.lua
@@ -1,46 +1,44 @@
-local lint = require "lint"
+local lint = require("lint")
 local linters = require("lint").linters
 
 lint.linters_by_ft = {
   python = { "flake8" },
   bash = { "shellcheck" },
   sql = { "sqlfluff" },
+  yaml = { "yamllint" },
+  -- yaml = { "actionlint" }, -- for gha
 }
 
-
 -- path/to/file:line:col: code message
-local pattern = "[^:]+:(%d+):(%d+):(%w+):(.+)"
-local groups = { "lnum", "col", "code", "message" }
+local pattern = '[^:]+:(%d+):(%d+):(%w+):(.+)'
+local groups = { 'lnum', 'col', 'code', 'message' }
 
--- set up flake8
 linters.flake8 = {
-  cmd = "flake8",
+  cmd = 'flake8',
   stdin = true,
   args = {
-    "--format=%(path)s:%(row)d:%(col)d:%(code)s:%(text)s",
-    "--no-show-source",
-    "--max-line-length=88",
-    "--stdin-display-name",
+    '--format=%(path)s:%(row)d:%(col)d:%(code)s:%(text)s',
+    '--no-show-source',
+    '--max-line-length=88', -- Set the character limit to 88
+    '--stdin-display-name',
     function()
       return vim.api.nvim_buf_get_name(0)
     end,
-    "-",
+    '-',
   },
   ignore_exitcode = true,
-  parser = require("lint.parser").from_pattern(pattern, groups, nil, {
-    ["source"] = "flake8",
-    ["severity"] = vim.diagnostic.severity.WARN,
+  parser = require('lint.parser').from_pattern(pattern, groups, nil, {
+    ['source'] = 'flake8',
+    ['severity'] = vim.diagnostic.severity.WARN,
   }),
 }
 
 -- set up sqlfluff
 linters.sqlfluff = {
-  cmd = "sqlfluff",
+  cmd = 'sqlfluff', -- Command to run the linter
   args = {
-    "lint",
-    "--format=json",
-    -- note: users will have to replace the --dialect argument accordingly
-    "--dialect=ansi",
+    "lint", "--format=json",
+    "--dialect=snowflake",
   },
   ignore_exitcode = true,
   stdin = false,
@@ -54,10 +52,10 @@ linters.sqlfluff = {
             filepath = "stdin",
             violations = {
               {
-                source = "sqlfluff",
+                source = 'sqlfluff',
                 line_no = 1,
                 line_pos = 1,
-                code = "jsonparsingerror",
+                code = 'jsonparsingerror',
                 description = output,
               },
             },
@@ -69,30 +67,32 @@ linters.sqlfluff = {
     end
     local diagnostics = {}
     for _, i_filepath in ipairs(per_filepath) do
-      for _, violation in ipairs(i_filepath.violations) do
-        table.insert(diagnostics, {
-          source = "sqlfluff",
-          lnum = (violation.line_no or violation.start_line_no) - 1,
-          col = (violation.line_pos or violation.start_line_pos) - 1,
-          severity = vim.diagnostic.severity.ERROR,
-          message = violation.description,
-          user_data = { lsp = { code = violation.code } },
-        })
-      end
+        for _, violation in ipairs(i_filepath.violations) do
+          table.insert(diagnostics, {
+            source = 'sqlfluff',
+            lnum = (violation.line_no or violation.start_line_no) - 1,
+            col = (violation.line_pos or violation.start_line_pos) - 1,
+            severity = vim.diagnostic.severity.ERROR,
+            message = violation.description,
+            user_data = {lsp = {code = violation.code}},
+          })
+        end
     end
     return diagnostics
   end,
 }
 
 
--- set up key mapping to trigger linting
+-- Set up key mapping to trigger linting
 vim.keymap.set("n", "<leader>l", function()
   lint.try_lint()
+  -- print("Triggering linting...")
 end, { desc = "Trigger linting for current file" })
 
--- Add autocmd to trigger linting on text change, insert leave and save
-vim.api.nvim_create_autocmd({ "BufWritePost", "TextChanged", "InsertLeave" }, {
+-- Add autocmd to trigger linting on text change and save
+vim.api.nvim_create_autocmd({"BufWritePost", "TextChanged", "InsertLeave"}, {
   callback = function()
+    -- require("lint").try_lint()
     lint.try_lint()
   end,
 })
