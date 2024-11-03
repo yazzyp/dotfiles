ea8d40e4675f4853a04b3f1b53941173ffd270e4
Yazz <67986029+yazzyp@users.noreply.github.com>
2024-11-03 15:28:40 +1100

    Update vim-slime.lua

--- .chezmoitemplates/nvim/lua/configs/vim-slime.lua
+++ .chezmoitemplates/nvim/lua/configs/vim-slime.lua
@@ -1,12 +1,30 @@
-vim.g.slime_input_pid = false
-vim.g.slime_suggest_default = true
-vim.g.slime_menu_config = false
-vim.g.slime_neovim_ignore_unlisted = false
--- options not set here are g:slime_neovim_menu_order, g:slime_neovim_menu_delimiter, and g:slime_get_jobid
--- see the documentation above to learn about those options
-
--- called MotionSend but works with textobjects as well
-vim.keymap.set("n", "gz", "<Plug>SlimeMotionSend", { remap = true, silent = false })
-vim.keymap.set("n", "gzz", "<Plug>SlimeLineSend", { remap = true, silent = false })
-vim.keymap.set("x", "gz", "<Plug>SlimeRegionSend", { remap = true, silent = false })
-vim.keymap.set("n", "gzc", "<Plug>SlimeConfig", { remap = true, silent = false })
+return {
+  "jpalardy/vim-slime",
+  -- lazy = false,
+  init = function()
+    -- these two should be set before the plugin loads
+    vim.g.slime_target = "neovim"
+    vim.g.slime_no_mappings = true
+  end,
+  config = function()
+    if vim.fn.confirm("Select target type", "&tmux\n&nvim", 2) == 1 then
+      vim.g.slime_target = "tmux"
+      vim.g.slime_default_config = {
+        socket_name = vim.api.nvim_eval('get(split($TMUX, ","), 0)'),
+        target_pane = "{top-right}",
+      }
+    else
+      vim.g.slime_input_pid = false
+      vim.g.slime_suggest_default = true
+      vim.g.slime_menu_config = false
+      vim.g.slime_neovim_ignore_unlisted = false
+    end
+  end,
+  keys = {
+    { "gz", "<Plug>SlimeMotionSend", mode = { "n" }, { remap = true, silent = false } },
+    { "gzz", "<Plug>SlimeLineSend", mode = { "n" }, { remap = true, silent = false } },
+    { "gzg", "<Plug>SlimeParagraphSend", mode = { "n" }, { remap = true, silent = false } },
+    { "gz", "<Plug>SlimeRegionSend", mode = { "x" }, { remap = true, silent = false } },
+    { "gzc", "<Plug>SlimeConfig", mode = { "n" }, { remap = true, silent = false } },
+  },
+}
