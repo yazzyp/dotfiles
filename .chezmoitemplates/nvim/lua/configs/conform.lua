e25dab7c61242c4643b137c4fa844d9f57a8ed46
yazzyp <yazz00@live.com.au>
2024-11-03 15:33:50 +1100

    Update .chezmoitemplates/nvim/lua/configs/conform.lua

--- .chezmoitemplates/nvim/lua/configs/conform.lua
+++ .chezmoitemplates/nvim/lua/configs/conform.lua
@@ -1,31 +1,31 @@
 local options = {
-  formatters_by_ft = {
-    lua = { "stylua" },
-    python = { "black" },
-    sql = { "sqlfmt" },
-    bash = { "shfmt" },
-    yaml = { "yamlfmt" },
-    cs = { "csharpier" },
-    json = { "prettier" },
-    -- css = { "prettier" },
-    -- html = { "prettier" },
-  },
+	formatters_by_ft = {
+		lua = { "stylua" },
+		python = { "black" },
+		sql = { "sqlfmt" },
+		bash = { "shfmt" },
+		yaml = { "yamlfmt" },
+		cs = { "csharpier" },
+		json = { "prettier" },
+		-- css = { "prettier" },
+		-- html = { "prettier" },
+	},
 
-  format_on_save = {
-    -- These options will be passed to conform.format()
-    lsp_fallback = true,
-    async = false,
-    timeout_ms = 500,
-  },
+	format_on_save = {
+		-- These options will be passed to conform.format()
+		lsp_fallback = true,
+		async = false,
+		timeout_ms = 500,
+	},
 
-  vim.keymap.set({ "n", "v" }, "<leader>fm", function()
-    local conform = require("conform")
-    conform.format({
-      lsp_fallback = true,
-      async = false,
-      timeout_ms = 1000000
-    })
-  end, {desc = "Format file or range (in visual mode)"})
+	vim.keymap.set({ "n", "v" }, "<leader>fm", function()
+		local conform = require("conform")
+		conform.format({
+			lsp_fallback = true,
+			async = false,
+			timeout_ms = 1000000,
+		})
+	end, { desc = "Format file or range (in visual mode)" }),
 }
 
 return options
