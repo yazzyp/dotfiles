0f758cf858305a0fb237195ddf655389a10db743
Yazz <67986029+yazzyp@users.noreply.github.com>
2024-11-03 15:31:51 +1100

    Update mappings.lua

--- .chezmoitemplates/nvim/lua/mappings.lua
+++ .chezmoitemplates/nvim/lua/mappings.lua
@@ -7,9 +7,12 @@ local map = vim.keymap.set
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
--- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
 
-vim.api.nvim_set_keymap("n", "<C-h>", "<cmd> TmuxNavigateLeft<CR>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "<C-l>", "<cmd> TmuxNavigateRight<CR>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "<C-j>", "<cmd> TmuxNavigateDown<CR>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "<C-k>", "<cmd> TmuxNavigateUp<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<C-h>', '<cmd> TmuxNavigateLeft<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<C-l>', '<cmd> TmuxNavigateRight<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<C-j>', '<cmd> TmuxNavigateDown<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<C-k>', '<cmd> TmuxNavigateUp<CR>', { noremap = true, silent = true })
+
+-- map('n', '<leader>o', '<cmd>lua vim.diagnostic.open_float()<CR>', { desc = "Open diagnostic float" })
+vim.keymap.set("n", "<leader>cz", "<cmd> Telescope chezmoi find_files <cr>", { desc = "Find chezmoi files" })
