911c6f7adbb712880c064031f8bf3670d211b143
yazzyp <67986029+yazzyp@users.noreply.github.com>
2026-07-13 20:08:35 +1000

    Update .chezmoitemplates/nvim/lua/autocmds.lua

--- .chezmoitemplates/nvim/lua/autocmds.lua
+++ .chezmoitemplates/nvim/lua/autocmds.lua
@@ -16,7 +16,8 @@ vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
     local edit_watch =
       function()
         require("chezmoi.commands.__edit").watch(bufnr)
-      end, vim.schedule(edit_watch)
+      end
+      vim.schedule(edit_watch)
   end,
 })
 
